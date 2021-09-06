13fa399123b4c3e506f914b4c37f5109a21c767e
BarinovIvan <pl_st@list.ru>
2021-09-06 10:44:32 +0300

    Задания 1,2

---
+++ src/Practise2/Ball.java
@@ -0,0 +1,27 @@
+package Practise2;
+
+public class Ball {
+    private String type;
+
+    public Ball() {
+    }
+
+    public Ball(String type) {
+        this.type = type;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    @Override
+    public String toString() {
+        return "Ball{" +
+                "type=" + type +
+                '}';
+    }
+}
---
+++ src/Practise2/Book.java
@@ -0,0 +1,38 @@
+package Practise2;
+
+public class Book {
+    private String author;
+    private String bookName;
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getBookName() {
+        return bookName;
+    }
+
+    public void setBookName(String bookName) {
+        this.bookName = bookName;
+    }
+
+    public Book(String author, String bookName) {
+        this.author = author;
+        this.bookName = bookName;
+    }
+
+    public Book() {
+    }
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "author=" + author +
+                ", bookName=" + bookName +
+                '}';
+    }
+}
