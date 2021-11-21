1c6a11b62482bf92c2607ea54eb598c989037e10
BarinovIvan <pl_st@list.ru>
2021-11-21 16:52:30 +0500

    Задания 25-28, отчеты внутри

---
+++ src/Practise25_26/Main.java
@@ -0,0 +1,9 @@
+package Practise25_26;
+
+import java.io.EOFException;
+
+public class Main {
+    public static void main(String[] args) throws EOFException {
+        Parser parser = new Parser();
+    }
+}
